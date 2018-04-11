9876b508d8a537a90bfd2baad1fc253ac47f2c64
Edlaine Zamora <ezamora@thoughtworks.com>
2018-04-10 23:41:01 -0300

    Created first scenario

---
+++ src/gatling/scala/test/PassengerSimulation.scala
@@ -0,0 +1,19 @@
+package test
+
+import io.gatling.http.Predef._
+import io.gatling.core.Predef._
+
+class PassengerSimulation extends Simulation {
+
+  val httpConfiguration = http.baseURL("https://airlineapi.herokuapp.com/")
+
+  val passengerScenario = scenario("Find Passengers")
+        .exec(http("Find all")
+          .get("passengers"))
+        .pause(5)
+
+  setUp(
+    passengerScenario.inject(atOnceUsers(1))
+  ).protocols(httpConfiguration)
+
+}
