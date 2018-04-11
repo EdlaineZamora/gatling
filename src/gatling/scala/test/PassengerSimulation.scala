ac47d387cf99c35b9908a5c07253145246ebf159
Edlaine Zamora <ezamora@thoughtworks.com>
2018-04-11 02:19:28 -0300

    Chagend user quantity

--- src/gatling/scala/test/PassengerSimulation.scala
+++ src/gatling/scala/test/PassengerSimulation.scala
@@ -13,7 +13,7 @@ class PassengerSimulation extends Simulation {
         .pause(5)
 
   setUp(
-    passengerScenario.inject(atOnceUsers(5))
+    passengerScenario.inject(atOnceUsers(6))
   ).protocols(httpConfiguration)
 
 }
